ec883a2be7795bb3bd508638bea2a8bd86b9a912
Navyasree08 <53593792+Navyasree08@users.noreply.github.com>
2024-10-28 17:16:34 +0530

    Update StringCalculatorFixture.Tests.cpp

--- StringCalculatorFixture.Tests.cpp
+++ StringCalculatorFixture.Tests.cpp
@@ -1,96 +1,39 @@
-#include "StringCalculator.h"
+#include "stringCalculator.h"
 #include <gtest/gtest.h>
 
-class StringCalculatorAddFixture: public testing::Test{
-  protected: 
+class string_calculator_add :  public ::testing::Test{
+protected:
     StringCalculator *objUnderTest;
-  void SetUp() override {
-  objUnderTest = new StringCalculator();
-  }
-  void TearDown(){
-    delete objUnderTest;
-  }
+    void SetUp() override
+    {
+      objUnderTest = new StringCalculator();
+    }
+    void TearDown() 
+    {
+      delete objUnderTest;
+    }
 };
 
-TEST_F(StringCalculatorAddFixture, Returns_0_For_Empty){
-  string input = "";
-  int expectedValue = 0;
-  int actualValue = objUnderTest->Add(input);
-  ASSERT_EQ(actualValue, expectedValue);
-}
+//TEST(TestSuiteName, TestCaseName)
+TEST_F(string_calculator_add,positive_cases){
+    ASSERT_EQ(objUnderTest->Add(""), 0);
+    ASSERT_EQ(objUnderTest->Add("0"), 0);
+    ASSERT_EQ(objUnderTest->Add("1"), 1);
+    ASSERT_EQ(objUnderTest->Add("1,2"), 3);
+    ASSERT_EQ(objUnderTest->Add("//;\n1;2"), 3);
+    ASSERT_EQ(objUnderTest->Add("42,1001,3",45);
+    ASSERT_EQ(objUnderTest->Add("//[***]\n8***2***3"), 13);
+    ASSERT_EQ(objUnderTest->Add("//[*][%]\n4*2%3"), 9);
+    ASSERT_EQ(objUnderTest->Add("//[**][%^]\n4**1%^9"), 14);
+  }
 
-TEST_F(StringCalculatorAddFixture, Returns_0_For_0){
-  string input = "0";
-  int expectedValue = 0;
-  int actualValue = objUnderTest->Add(input);
-  ASSERT_EQ(actualValue, expectedValue);
-}
-
-TEST_F(StringCalculatorAddFixture, Returns_1_For_1){
-  string input = "1";
-  int expectedValue = 1;
-  int actualValue = objUnderTest->Add(input);
-  ASSERT_EQ(actualValue, expectedValue);
-}
-
-TEST_F(StringCalculatorAddFixture, two_comma_delimited_numbers){
-  string input = "1,2";
-  int expectedValue = 3;
-  int actualValue = objUnderTest->Add(input);
-  ASSERT_EQ(actualValue, expectedValue);
-}
-
-TEST_F(StringCalculatorAddFixture, delimited_with_newline_and_comma){
-  string input = "1\n2,5";
-  int expectedValue = 8;
-  int actualValue = objUnderTest->Add(input);
-  ASSERT_EQ(actualValue, expectedValue);
-}
-
-TEST_F(StringCalculatorAddFixture, delimiter){
-  string input = "//;\n2,5";
-  int expectedValue = 7;
-  int actualValue = objUnderTest->Add(input);
-  ASSERT_EQ(actualValue, expectedValue);
-}
-
-TEST_F(StringCalculatorAddFixture, Numbers_above_1000){
-  string input = "42,1001,3";
-  int expectedValue = 45;
-  int actualValue = objUnderTest->Add(input);
-  ASSERT_EQ(actualValue, expectedValue);
-}
-
-TEST_F(StringCalculatorAddFixture, Multicharacter_delimiter){
-  string input = "//[***]\n8***2***3";
-  int expectedValue = 13;
-  int actualValue = objUnderTest->Add(input);
-  ASSERT_EQ(actualValue, expectedValue);
-}
-
-TEST_F(StringCalculatorAddFixture, Multiple_delimiter){
-  string input = "//[*][%]\n4*2%3";
-  int expectedValue = 9;
-  int actualValue = objUnderTest->Add(input);
-  ASSERT_EQ(actualValue, expectedValue);
-}
-
-TEST_F(StringCalculatorAddFixture, Multiple_Multicharacter_delimiter){
-  string input = "//[**][%^]\n4**1%^9";
-  int expectedValue = 14;
-  int actualValue = objUnderTest->Add(input);
-  ASSERT_EQ(actualValue, expectedValue);
-}
-TEST_F(StringCalculatorAddFixture, negative_numbers){
-  string input = "1,-2,-4,5";
-  ASSERT_THROW(objUnderTest->Add(input), std::invalid_argument);
-}
-
-TEST_F(StringCalculatorAddFixture, negative_numbers_Try_catch){
-  string input = "1,-2,-4,5";
+TEST_F(string_calculator_add, ThrowsExceptionForNegativeNumbers) {
     try {
-        objUnderTest->Add(input);
-    } catch (const std::invalid_argument& e) {
-        EXPECT_STREQ(e.what(), "Negatives not allowed: -2,-4");
+        objUnderTest->Add("1,-2,-4,5");
+    } catch (std::invalid_argument const &err) {
+        ASSERT_EQ(err.what(), std::string("Negatives not allowed: -2,-4"));
+    } catch (...) {
+        FAIL() << "Expected std::invalid_argument";
     }
 }
+    
