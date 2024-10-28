2b46539fba51445b25eb41b83170114dd0a8966e
Navyasree08 <53593792+Navyasree08@users.noreply.github.com>
2024-10-28 15:09:46 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,11 +1,28 @@
 #include "StringCalculator.h"
 #include <gtest/gtest.h>
 
-TEST(string_calculator_ass,when_passed_a_single_number_returns_0_for_Empty){
+TEST(string_calculator_ass,when_passed_a_single_number_returns_0){
 //Arrange
   StringCalculator  objUnderTests;
   string input = "";
   int expectedValue = 0;
+
+
+  //ACT
+  int actualValue = objUnderTests.Add(input);
+
+  //ASSERT
+  ASSERT_EQ(actualValue,expectedValue);
+  input = "0";
+  actualValue = objUnderTests.Add(input);
+  ASSERT_EQ(actualValue,expectedValue);
+}
+
+TEST(string_calculator_ass,when_passed_a_single_number_returns_0_for_1){
+//Arrange
+  StringCalculator  objUnderTests;
+  string input = "1";
+  int expectedValue = 1;
 
 
   //ACT
