77646e1aa2b56d37f6103dc673a1943e2b895de0
Navyasree08 <53593792+Navyasree08@users.noreply.github.com>
2024-10-28 17:21:39 +0530

    Update StringCalculatorFixture.Tests.cpp

--- StringCalculatorFixture.Tests.cpp
+++ StringCalculatorFixture.Tests.cpp
@@ -25,15 +25,15 @@ TEST_F(string_calculator_add,positive_cases){
     ASSERT_EQ(objUnderTest->Add("//[***]\n8***2***3"), 13);
     ASSERT_EQ(objUnderTest->Add("//[*][%]\n4*2%3"), 9);
     ASSERT_EQ(objUnderTest->Add("//[**][%^]\n4**1%^9"), 14);
-  }
+}
 
 TEST_F(string_calculator_add, ThrowsExceptionForNegativeNumbers) {
+    objUnderTest->Add("1,-2,-4,5");
+    ASSERT_THROW(objUnderTest->Add("1,-2,-4,5"), std::invalid_argument);
     try {
         objUnderTest->Add("1,-2,-4,5");
-    } catch (std::invalid_argument const &err) {
-        ASSERT_EQ(err.what(), std::string("Negatives not allowed: -2,-4"));
-    } catch (...) {
-        FAIL() << "Expected std::invalid_argument";
+    } catch (const std::invalid_argument& e) {
+        EXPECT_STREQ(e.what(), "Negatives not allowed: -2,-4");
     }
 }
     
